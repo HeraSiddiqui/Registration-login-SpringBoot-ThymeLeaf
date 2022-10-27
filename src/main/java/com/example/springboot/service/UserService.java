e8d8f106314e513c4ba173cb16f657a1dc74b6d7
unknown <HeraSiddiqui>
2022-10-27 21:37:21 +0530

    Registration&Login-SpringBoot+Thymeleaf

---
+++ src/main/java/com/example/springboot/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.springboot.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.example.springboot.model.User;
+import com.example.springboot.web.dto.UserRegistrationDto;
+
+public interface UserService extends UserDetailsService {
+		User save(UserRegistrationDto registrationDto);
+	}
